f484d3966c65ef625b698bf8a04f0ee3e794f55b
Mampuru <mampuruntwampe@gmail.com>
2023-12-14 23:39:09 +0200

    Changed material to getMaterial  app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:verbal_fluency/views/recording_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Verbel Flex',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Placeholder(),
+      home: RecordingView(),
     );
   }
 }

b99e1e65aad0a1f41ad64560c75715dea48ebdb3
Mampuru <mampuruntwampe@gmail.com>
2023-12-19 23:57:38 +0200

    Addition of the result view to display score

---
+++ lib/views/result_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ScoreDisplay extends StatelessWidget {
+  final String recording;
+  final int score;
+
+  const ScoreDisplay({
+    required this.recording,
+    required this.score,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(recording),
+      subtitle: Text('Score: $score'),
+    );
+  }
+}

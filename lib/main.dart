6dd036cf426fdb351a7fefc65a76d43dd4929f78
Mampuru <mampuruntwampe@gmail.com>
2024-03-18 23:58:46 +0200

    App name fix in the app title

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Verbel Flex',
+      title: 'Verbal Flex',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
